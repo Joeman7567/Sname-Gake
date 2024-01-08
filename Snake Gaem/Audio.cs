b99cd87950b491bbc2cf0867e62e9a720f05ac3d
22003366ctc <22003366ctc@MA-CTC-INST-S08>
2024-01-08 11:42:52 -0500

    Final

---
+++ Snake Gaem/Audio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Media;
+/*namespace Snake_Gaem
+{
+    public static class Audio
+    {
+        public readonly static MediaPlayer fortnut =
+            LoadAudio("fortnut.mp3");
+
+        private static MediaPlayer LoadAudio(string filename, bool repeat = false)
+        {
+            MediaPlayer player = new();
+            player.Open(new Uri($"Assets/{filename}", UriKind.Relative));
+            return player;
+        }
+    }
+}*/
---
+++ Snake Gaem/GameState.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Markup;
+
+namespace Snake_Gaem
+{
+    public class GameState
+    {
+        public int Rows { get; }
+        public int Cols { get; }
+        public GridValue[,] Grid { get; }
+        public Direction Dir { get; private set; }
+        public int Score { get; private set; }
+        public bool GameOver { get; private set; }
+
+        private readonly LinkedList<Direction> dirChanges = new LinkedList<Direction>();
+        private readonly LinkedList<Position> snakePositions = new LinkedList<Position>();
+        private readonly Random random = new Random();
+
+        public GameState(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            Grid = new GridValue[Rows, Cols];
+            Dir = Direction.Right;
+
+            AddSnake();
+            AddFood();
+           // AddGun();
+        }
+
+        private void AddSnake()
+        {
+            int r = Rows / 2;
+
+
+            for (int c = 1; c <= 3; c++)
+            {
+                Grid[r, c] = GridValue.Snake;
+                snakePositions.AddFirst(new Position(r, c));
+            }
+
+        }
+
+        private IEnumerable<Position> EmptyPositions()
+        {
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Cols; c++)
+                {
+                    if (Grid[r, c] == GridValue.Empty)
+                    {
+                        yield return new Position(r, c);
+                    }
+                }
+
+            }
+        }
+
+        private void AddFood()
+        {
+            List<Position> empty = new List<Position>(EmptyPositions());
+
+            if (empty.Count == 0)
+            {
+                return;
+            }
+            for (int i = 0; i < 10 ; i++)
+            {
+                Position pos = empty[random.Next(empty.Count)];
+                Grid[pos.Row, pos.Col] = GridValue.Food;
+            }
+           
+        }
+
+       /* private void AddGun()
+        {
+            List<Position> empty = new List<Position>(EmptyPositions());
+
+            if (empty.Count == 0)
+            {
+                return;
+            }
+
+            Position pos = empty[random.Next(empty.Count)];
+            Grid[pos.Row, pos.Col] = GridValue.gun;
+        }*/
+
+        public Position HeadPosition()
+        {
+            return snakePositions.First.Value;
+        }
+
+        public Position TailPosition()
+        {
+            return snakePositions.Last.Value;
+        }
+
+        public IEnumerable<Position> SnakePositions()
+        {
+            return snakePositions;
+        }
+
+        private void AddHead(Position pos)
+        {
+            snakePositions.AddFirst(pos);
+            Grid[pos.Row, pos.Col] = GridValue.Snake;
+
+        }
+
+        private void RemoveTail()
+        {
+            Position tail = snakePositions.Last.Value;
+            Grid[tail.Row, tail.Col] = GridValue.Empty;
+            snakePositions.RemoveLast();
+        }
+
+        private Direction GetLastDirection()
+        {
+            if (dirChanges.Count == 0)
+            {
+                return Dir;
+            }
+
+            return dirChanges.Last.Value;
+        }
+
+        private bool CanChangeDirection(Direction newDir)
+        {
+            if (dirChanges.Count ==2)
+            {
+                return false;
+            }
+
+            Direction lastDir = GetLastDirection();
+            return newDir != lastDir && newDir != lastDir.Opposite();
+        }
+
+        public void ChangeDirection(Direction dir)
+        {
+            if (CanChangeDirection(dir))
+            {
+                dirChanges.AddLast(dir);
+            }
+            
+        }
+
+        private bool OutsideGrid(Position pos)
+        {
+            return pos.Row < 0 || pos.Row >= Rows || pos.Col < 0 || pos.Col >= Cols;
+        }
+
+        private GridValue WillHit(Position newHeadPos)
+        {
+
+            if (OutsideGrid(newHeadPos))
+            {
+                return GridValue.Outside;
+
+            }
+
+            if (newHeadPos == TailPosition())
+            {
+                return GridValue.Empty;
+
+            }
+
+            return Grid[newHeadPos.Row, newHeadPos.Col];
+        }
+
+        public void Move()
+        {
+            if (dirChanges.Count > 0)
+            {
+                Dir = dirChanges.First.Value;
+                dirChanges.RemoveFirst();
+            }
+
+            Position newHeadPos = HeadPosition().Translate(Dir);
+            GridValue hit = WillHit(newHeadPos);
+
+            if (hit == GridValue.Outside || hit == GridValue.Snake)
+            {
+                GameOver = true;
+            }
+            else if (hit == GridValue.Empty)
+            {
+                RemoveTail();
+                AddHead(newHeadPos);
+            }
+            else if (hit == GridValue.Food)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\22003366ctc\Source\Repos\Sname-Gake\Snake Gaem\Assets\apple-munch-40169.wav");
+                player.Play();
+                AddHead(newHeadPos);
+                Score++;
+                AddFood();
+            }
+           // else if (hit == GridValue.gun)
+         //   {
+
+        //    }
+        }
+    }
+}
